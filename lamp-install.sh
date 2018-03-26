efe0d909748dc2e58d24b8b79a6e2eced380da8e
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2018-03-26 19:54:13 -0300

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -31,11 +31,43 @@ apt-get install -f
 echo "#------------------------------------------------------------#"
 echo "Configuring mysql server to use MYSQLPASS123 as root password"
 echo "#------------------------------------------------------------#"
-echo debconf mysql-server/root_password password MYSQLPASS123 | sudo debconf-set-selections
-echo debconf mysql-server/root_password_again password MYSQLPASS123 | sudo debconf-set-selections
+MYSQL_ROOT_PASSWORD='MYSQLPASS123'
+
+echo debconf mysql-server/root_password password $MYSQL_ROOT_PASSWORD | sudo debconf-set-selections
+echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD | sudo debconf-set-selections
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYSQLPASS123'
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYSQLPASS123'
 apt-get -y install mysql-server
+
+tee ~/secure_our_mysql.sh > /dev/null << EOF
+spawn $(which mysql_secure_installation)
+
+expect "Enter password for user root:"
+send "$MYSQL_ROOT_PASSWORD\r"
+
+expect "Press y|Y for Yes, any other key for No:"
+send "y\r"
+
+expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
+send "2\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "n\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+EOF
+
 
 # RESTART SERVICES
 echo -e "\n"
