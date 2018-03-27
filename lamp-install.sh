742b71dd9b16617087aafaba6f1f117503f34a61
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2018-03-26 21:57:15 -0300

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -20,7 +20,7 @@ systemclt start apache2
 systemclt enable apache2
 
 #PHP MYSQL 
-apt-get install -y install -y apache2 mariadb-server libapache2-mod-php7.0 \
+apt-get install -y apache2 mariadb-server libapache2-mod-php7.0 \
     php7.0-gd php7.0-json php7.0-mysql php7.0-curl \
     php7.0-intl php7.0-mcrypt php-imagick \
     php7.0-zip php7.0-xml php7.0-mbstring
